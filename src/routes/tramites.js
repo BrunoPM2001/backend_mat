50b9c53ed741c114ccd5f20fa2386e941a4ad088
Bruno Porras <brunoporras2001@gmail.com>
2023-10-06 15:41:14 -0500

    Routes to 'tramites' and 'usuarios' mapped

---
+++ src/routes/tramites.js
@@ -0,0 +1,34 @@
+import { Router } from "express"
+
+const router = Router()
+
+//  Rutas
+router.get("/getTramites", (req, res) => {
+  res.json({ message: "Trámites" })
+})
+
+router.get("/getTramite", (req, res) => {
+  res.json({ message: "Trámite" })
+})
+
+router.get("/getStatsTramite", (req, res) => {
+  res.json({ message: "Estadísticas del trámite" })
+})
+
+router.post("/createTramite", (req, res) => {
+  res.json({ message: "Trámite creado" })
+})
+
+router.put("/updateTramite", (req, res) => {
+  res.json({ message: "Trámite actualizado" })
+})
+
+router.put("/disableTramite", (req, res) => {
+  res.json({ message: "Trámite habilitado" })
+})
+
+router.put("/enableTramite", (req, res) => {
+  res.json({ message: "Trámite deshabilitado" })
+})
+
+export default router
---
+++ src/routes/usuarios.js
@@ -0,0 +1,25 @@
+import { Router } from "express"
+import ctrl from "../controllers/usuario.controller.js"
+
+const router = Router()
+
+//  Rutas
+router.get("/getUsuarios", ctrl.getUsuarios)
+
+router.get("/getUsuario", ctrl.getUsuario)
+
+router.post("/createUsuario", ctrl.createUsuario)
+
+router.put("/updateUsuario", (req, res) => {
+  res.json({ message: "Usuario actualizado" })
+})
+
+router.put("/disableUsuario", ctrl.disableUsuario)
+
+router.put("/enableUsuario", ctrl.enableUsuario)
+
+router.put("/changePass", ctrl.changePass)
+
+router.put("/restorePass", ctrl.restorePass)
+
+export default router

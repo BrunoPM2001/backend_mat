6d0c8b4eb71497f319789e5eb9bd4e0f12baa9f3
Bruno Porras <brunoporras2001@gmail.com>
2023-10-06 15:44:59 -0500

    Entrypoint

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi mom")
+import express from "express"
+import cors from "cors"
+import router_usuarios from "./src/routes/usuarios.js"
+import router_tramites from "./src/routes/tramites.js"
+
+const app = express()
+
+//  Middlewares
+app.use(cors())
+app.use(express.json())
+
+//  Rutas
+app.use('/usuarios', router_usuarios)
+app.use('/tramites', router_tramites)
+
+//  Corriendo servicio
+app.listen(3000, () => console.log("Listening..."))

4948bb78c717e96521b5632e53edd3e3d402ccf3
BrunoPM <brunoporras2001@gmail.com>
2023-10-09 14:55:52 -0500

    Routes added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import router_usuarios from "./src/routes/usuarios.js";
+import router_dependencias from "./src/routes/dependencias.js";
+import router_categoriasTramite from "./src/routes/categoriasTramite.js";
 import router_tramites from "./src/routes/tramites.js";
+import router_requisitos from "./src/routes/requisitosTramite.js";
 
 const app = express();
 
@@ -11,7 +14,10 @@ app.use(express.json());
 
 //  Rutas
 app.use("/usuarios", router_usuarios);
+app.use("/dependencias", router_dependencias);
+app.use("/categoriasTramite", router_categoriasTramite);
 app.use("/tramites", router_tramites);
+app.use("/requisitos", router_requisitos);
 
 //  Corriendo servicio
 app.listen(3000, () => console.log("Listening..."));

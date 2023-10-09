431665d48a957fbcc7fff3b3d0092131f3f5b606
BrunoPM <brunoporras2001@gmail.com>
2023-10-09 09:31:16 -0500

    Formating code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from "express"
-import cors from "cors"
-import router_usuarios from "./src/routes/usuarios.js"
-import router_tramites from "./src/routes/tramites.js"
+import express from "express";
+import cors from "cors";
+import router_usuarios from "./src/routes/usuarios.js";
+import router_tramites from "./src/routes/tramites.js";
 
-const app = express()
+const app = express();
 
 //  Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //  Rutas
-app.use('/usuarios', router_usuarios)
-app.use('/tramites', router_tramites)
+app.use("/usuarios", router_usuarios);
+app.use("/tramites", router_tramites);
 
 //  Corriendo servicio
-app.listen(3000, () => console.log("Listening..."))
+app.listen(3000, () => console.log("Listening..."));

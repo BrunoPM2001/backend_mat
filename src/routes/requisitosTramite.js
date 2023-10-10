f6c239af2b98dffa69e610e5b334caa141b42b71
Bruno Porras <brunoporras2001@gmail.com>
2023-10-10 13:29:39 -0500

    Middlewares to verify the load of files added

--- src/routes/requisitosTramite.js
+++ src/routes/requisitosTramite.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import ctrl from "../controllers/requisitosTramite.controller.js";
+import { upload, verifySize } from "../../config/multer.js";
 
 const router = Router();
 
 //  Rutas
 router.get("/getRequisitosTramite", ctrl.getRequisitosTramite);
 
-router.post("/createRequisitoTramite", ctrl.createRequisitoTramite);
+router.post("/createRequisitoTramite", upload.single('file'), verifySize, ctrl.createRequisitoTramite);
 
 router.put("/disableRequisitoTramite", ctrl.disableRequisitoTramite);
 

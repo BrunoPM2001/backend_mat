03bf10cac5ceed27a5ca2fcbb571213d871c429a
Bruno Porras <brunoporras2001@gmail.com>
2023-10-11 11:32:44 -0500

    Routes sample for 'Soliciudes'

---
+++ src/routes/solicitudes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+//import ctrl from "../controllers/solicitudes.controller.js";
+
+const router = Router();
+
+//  Rutas
+router.get("/getSolicitudes", (req, res) => {
+  res.json({ message: "Success", data: "Solicitudes" })
+});
+
+router.post("/createSolicitud", (req, res) => {
+  res.json({ message: "Success", data: "Crear solicitud" })
+});
+
+router.put("/subsanarSolicitud", (req, res) => {
+  res.json({ message: "Success", data: "Subsanar solicitud" })
+});
+
+router.delete("/cancelarTramite", (req, res) => {
+  res.json({ message: "Success", data: "Solicitud eliminada" })
+});
+
+export default router;

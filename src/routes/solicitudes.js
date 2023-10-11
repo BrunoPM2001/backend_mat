65269b0b388f40e98439262509ba936826247066
Bruno Porras <brunoporras2001@gmail.com>
2023-10-11 17:06:13 -0500

    Multer updated

--- src/routes/solicitudes.js
+++ src/routes/solicitudes.js
@@ -1,16 +1,24 @@
 import { Router } from "express";
-//import ctrl from "../controllers/solicitudes.controller.js";
+import ctrl from "../controllers/solicitudes.controller.js";
+import { uploadRequisitos, verifySize } from "../../config/multer.js";
 
 const router = Router();
 
 //  Rutas para user
-router.get("/getSolicitudes", (req, res) => {
-  res.json({ message: "Success", data: "Solicitudes" })
-});
+router.get("/getSolicitudes", ctrl.getSolicitudes);
 
-router.post("/createSolicitud", (req, res) => {
-  res.json({ message: "Success", data: "Crear solicitud" })
-});
+router.post("/createSolicitud", uploadRequisitos.fields([
+  {name: "requisito_1", maxCount: 1},
+  {name: "requisito_2", maxCount: 1},
+  {name: "requisito_3", maxCount: 1},
+  {name: "requisito_4", maxCount: 1},
+  {name: "requisito_5", maxCount: 1},
+  {name: "requisito_6", maxCount: 1},
+  {name: "requisito_7", maxCount: 1},
+  {name: "requisito_8", maxCount: 1},
+  {name: "requisito_9", maxCount: 1},
+  {name: "requisito_10", maxCount: 1},
+]),ctrl.createSolicitud);
 
 router.put("/subsanarSolicitud", (req, res) => {
   res.json({ message: "Success", data: "Subsanar solicitud" })

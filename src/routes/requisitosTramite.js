ce16768ff9df99b704348255946a57195607796f
Bruno Porras <brunoporras2001@gmail.com>
2023-10-11 17:05:55 -0500

    Routes updated

--- src/routes/requisitosTramite.js
+++ src/routes/requisitosTramite.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import ctrl from "../controllers/requisitosTramite.controller.js";
-import { upload, verifySize } from "../../config/multer.js";
+import { uploadPlantillas, verifySize } from "../../config/multer.js";
 
 const router = Router();
 
@@ -9,9 +9,9 @@ router.get("/getRequisitosTramiteAdmin", ctrl.getRequisitosTramiteAdmin);
 
 router.get("/getRequisitosTramite", ctrl.getRequisitosTramite);
 
-router.post("/createRequisitoTramite", upload.single('file'), verifySize, ctrl.createRequisitoTramite);
+router.post("/createRequisitoTramite", uploadPlantillas.single('file'), verifySize, ctrl.createRequisitoTramite);
 
-router.put("/updatePlantillaRequisito", upload.single('file'), verifySize, ctrl.updatePlantillaRequisito)
+router.put("/updatePlantillaRequisito", uploadPlantillas.single('file'), verifySize, ctrl.updatePlantillaRequisito)
 
 router.put("/disableRequisitoTramite", ctrl.disableRequisitoTramite);
 

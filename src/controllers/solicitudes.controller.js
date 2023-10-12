4cd9f8d8a4efb41c4295e896cb7095b7d4268f14
Bruno Porras <brunoporras2001@gmail.com>
2023-10-12 11:59:29 -0500

    Load multiple files and remove multiple files ok

--- src/controllers/solicitudes.controller.js
+++ src/controllers/solicitudes.controller.js
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { DeleteObjectCommand, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
+import { DeleteObjectCommand, PutObjectCommand, GetObjectCommand, DeleteObjectsCommand } from "@aws-sdk/client-s3"
 import s3Client from "../../config/s3client.js"
 
 const prisma = new PrismaClient();
 const ctrl = {};
 
-//  Funciones * TODO - Use JWT
+//  Funciones * TODO - Use JWT - Validar tipo de archivo cargado
 ctrl.getSolicitudes = async (req, res) => {
   try {
     const { id } = req.query;
@@ -36,7 +36,7 @@ ctrl.createSolicitud = async (req, res) => {
         activo: true
       }
     });
-    //  Validar: Obligatorio? => Tamaño máx? => Subir archivo
+    //  Validar: Obligatorio? => Tipo de archivo => Tamaño máx? => Subir archivo
     for (let i = 0; i < req_tramite.length; i++) {
       let req = req_tramite[i];
       let file = files["requisito_" + (i + 1)];
@@ -52,14 +52,73 @@ ctrl.createSolicitud = async (req, res) => {
         return;
       }
     }
-    //  Crear registros en la DB y subir elementos a minio
-    validFiles.forEach(i => {
-      
+    //  Crear solicitud en la DB
+    const solicitud = await prisma.solicitudes.create({
+      data: {
+        id_tramite: Number(id_tramite),
+        id_usuario: Number(id_usuario)
+      }
+    });
+    validFiles.forEach(async(i) => {
+      let req = req_tramite[i];
+      let fileName = "requisito_" + (i + 1);
+      let file = files[fileName][0];
+      //  Subir registro a la DB
+      await prisma.requisitosSolicitud.create({
+        data: {
+          id_solicitud: Number(solicitud.id),
+          id_requisito: req.id,
+          nombreArchivo: fileName
+        }
+      });
+      // Subir requisito a minio
+      await s3Client.send(new PutObjectCommand({
+        Bucket: process.env.BUCKET_REQUISITOS_SOLICITUD,
+        Key: solicitud.id + "/" + fileName,
+        Body: file.buffer
+      }));
     })
 
-    res.json({ cuenta: req_tramite })
+    res.json({ message: "Success", data: solicitud });
   } catch (e) {
-    
+    console.log(e);
+    res.json({ message: "Fail", data: "Exception" });
+  }
+}
+
+ctrl.deleteSolicitud = async (req, res) => {
+  try {
+    const { id, id_usuario } = req.query;
+    const estado = await prisma.solicitudes.findUnique({
+      where: {
+        id: Number(id),
+        id_usuario: Number(id_usuario)
+      }
+    });
+    if (estado.estado == "Pendiente") {
+      //  Eliminar solicitud
+      const reqDel = await prisma.solicitudes.delete({
+        where: {
+          id: Number(id)
+        },
+        include: {
+          RequisitosSolicitud: true
+        }
+      });
+      //  Eliminar archivos cargados
+      reqDel.RequisitosSolicitud.forEach(async(ele) => {
+        await s3Client.send(new DeleteObjectCommand({
+          Bucket: process.env.BUCKET_REQUISITOS_SOLICITUD,
+          Key: reqDel.id + "/" + "requisito_" + ele.id_requisito
+        }))
+      });
+      res.json({ message: "Success", data: "Solicitud eliminada correctamente!" });
+    } else {
+      res.json({ message: "Fail", data: "No se puede eliminar la solicitud!" });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ message: "Fail", data: "Exception" });
   }
 }
 

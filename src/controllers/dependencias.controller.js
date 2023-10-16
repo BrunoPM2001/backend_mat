56527965e5a1f481c1c18ad6a97e79cd7bd145b1
Bruno Porras <brunoporras2001@gmail.com>
2023-10-16 17:42:27 -0500

    JWT include in every controller

--- src/controllers/dependencias.controller.js
+++ src/controllers/dependencias.controller.js
@@ -1,13 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 const ctrl = {};
 
-//  Funciones - TODO * USE JWT
+//  Funciones - TODO
 ctrl.getDependencias = async (req, res) => {
   try {
-    const result = await prisma.dependencias.findMany();
-    res.json({ message: "Success", data: result });
+    const token = req.header("Authorization");
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      if (err) {
+        res.json({ message: "Fail", data: "Error en token" });
+      } else {
+        const result = await prisma.dependencias.findMany();
+        res.json({ message: "Success", data: result });
+      }
+    });
   } catch (e) {
     console.log(e);
     res.json({ message: "Fail", data: "Exception" });
@@ -16,13 +24,25 @@ ctrl.getDependencias = async (req, res) => {
 
 ctrl.createDependencia = async (req, res) => {
   try {
-    const { nombre } = req.body;
-    const result = await prisma.dependencias.create({
-      data: {
-        nombre: nombre,
-      },
+    const token = req.header("Authorization");
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      if (err) {
+        res.json({ message: "Fail", data: "Error en token" });
+      } else {
+        //  Validar permisos del usuario
+        if (payload.permisos >= 4) {
+          const { nombre } = req.body;
+          const result = await prisma.dependencias.create({
+            data: {
+              nombre: nombre,
+            },
+          });
+          res.json({ message: "Success", data: result });
+        } else {
+          res.json({ message: "Fail", data: "Permisos insuficientes" });
+        }
+      }
     });
-    res.json({ message: "Success", data: result });
   } catch (e) {
     console.log(e);
     res.json({ message: "Fail", data: "Exception" });

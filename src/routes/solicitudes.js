eaa25fae68ac44cb904b3ae31b1d79cf7321cd55
Bruno Porras <brunoporras2001@gmail.com>
2023-10-18 12:23:45 -0500

    Route to get files from S3 added

--- src/routes/solicitudes.js
+++ src/routes/solicitudes.js
@@ -7,21 +7,27 @@ const router = Router();
 //  Rutas para user - TODO estimar límite de requisitos - IMPLEMENTAR SUBSANAR
 router.get("/getSolicitudes", ctrl.getSolicitudes);
 
-router.post("/createSolicitud", uploadRequisitos.fields([
-  {name: "requisito_1", maxCount: 1},
-  {name: "requisito_2", maxCount: 1},
-  {name: "requisito_3", maxCount: 1},
-  {name: "requisito_4", maxCount: 1},
-  {name: "requisito_5", maxCount: 1},
-  {name: "requisito_6", maxCount: 1},
-  {name: "requisito_7", maxCount: 1},
-  {name: "requisito_8", maxCount: 1},
-  {name: "requisito_9", maxCount: 1},
-  {name: "requisito_10", maxCount: 1},
-]), ctrl.createSolicitud);
+router.get("/getAdjuntos", ctrl.getAdjuntosSolicitud);
+
+router.post(
+  "/createSolicitud",
+  uploadRequisitos.fields([
+    { name: "requisito_1", maxCount: 1 },
+    { name: "requisito_2", maxCount: 1 },
+    { name: "requisito_3", maxCount: 1 },
+    { name: "requisito_4", maxCount: 1 },
+    { name: "requisito_5", maxCount: 1 },
+    { name: "requisito_6", maxCount: 1 },
+    { name: "requisito_7", maxCount: 1 },
+    { name: "requisito_8", maxCount: 1 },
+    { name: "requisito_9", maxCount: 1 },
+    { name: "requisito_10", maxCount: 1 },
+  ]),
+  ctrl.createSolicitud
+);
 
 router.put("/subsanarSolicitud", (req, res) => {
-  res.json({ message: "Success", data: "Subsanar solicitud" })
+  res.json({ message: "Success", data: "Subsanar solicitud" });
 });
 
 router.delete("/deleteSolicitud", ctrl.deleteSolicitud);
